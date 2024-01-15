1040546259f784cf519f7ca12aeb3d7163e82477
s2322013 <s2322013@stu.musashino-u.ac.jp>
2024-01-15 13:56:54 +0900

    データベース保存

--- saisyuu.py
+++ saisyuu.py
@@ -11,3 +11,40 @@ html_content = response.content
 
 # BeautifulSoupオブジェクトを作成
 soup = BeautifulSoup(html_content, 'html.parser')
+
+# データベース接続を作成
+conn = sqlite3.connect('weather_data.db')
+
+# カーソルオブジェクトを作成
+cursor = conn.cursor()
+
+# テーブルを作成
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS weather (
+    date TEXT,
+    pressure TEXT,
+    rainfall TEXT,
+    temp_avg TEXT,
+    temp_high TEXT,
+    temp_low TEXT,
+    vapor_pressure TEXT,
+    humidity TEXT
+)
+''')
+
+# コミットして変更を確定
+conn.commit()
+
+
+# データをデータベースに挿入
+for data in data_list:
+    cursor.execute('''
+    INSERT INTO weather (date, pressure, rainfall, temp_avg, temp_high, temp_low, vapor_pressure, humidity)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', data)
+
+# コミットして変更を確定
+conn.commit()
+
+# データベース接続を閉じる
+conn.close()
